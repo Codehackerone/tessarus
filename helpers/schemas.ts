bfe6fd7b88c9bd79bf4f377cca02cc99c7a44bbe
Soumyajit Datta <soumyajitdatta123@gmail.com>
2023-01-26 11:18:45 +0530

    user joi validationadded

---
+++ helpers/schemas.ts
@@ -0,0 +1,38 @@
+import BaseJoi from "joi";
+import sanitizeHtml from "sanitize-html";
+
+const extension = (joi: any) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value: any, helpers: any) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
+
+export const userRegistrationschema = Joi.object({
+  name: Joi.string().required().escapeHTML(),
+  email: Joi.string().required().escapeHTML(),
+  phone: Joi.string().required().escapeHTML(),
+  password: Joi.string().required().escapeHTML(),
+  gender: Joi.string().valid("Male", "Female", "Others"),
+  dateOfBirth: Joi.date(),
+  college: Joi.string().required().escapeHTML(),
+  degree: Joi.string().valid("B.Tech", "M.Tech", "MCA"),
+  year: Joi.string(),
+  stream: Joi.string(),
+});
--- models/user.model.ts
+++ models/user.model.ts
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   },
-  espektro_id: {
+  espektroId: {
     type: String,
     required: true,
   },
